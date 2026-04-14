83e3f7604db9fa17fd937deafa016ee3fb4188d2
LourencoFreire <lourencofreire3@gmail.com>
2026-04-14 21:20:09 +0100

    add subtraction (i didnt just copy paste the add func)

--- main.c
+++ main.c
@@ -12,14 +12,29 @@ void add()
     printf("%lf + %lf = %lf\n", a1, a2, answerA);
 }
 
+void subtract()
+{
+    double s1, s2, answerS;
+    printf("Enter the first number:\n");
+    scanf("%lf", &s1);
+    printf("Enter the second number:\n");
+    scanf("%lf", &s2);
+    answerS = s1 - s2;
+    printf("%lf + %lf = %lf\n", s1, s2, answerS);
+}
+
 int main()
 {
     char userInput;
-    printf("What arithmetic operation would u like to perform? (+)\n");
+    printf("What arithmetic operation would u like to perform? (+) (-)\n");
     scanf(" %c", &userInput);
     if (userInput == '+')
     {
         add();
+    }
+    else if (userInput == '-')
+    {
+        subtract();
     }
     else
     {
